37dbbf1c96291d353e4e913cd63a08cdb0d19345
Shane Harris <shane@xactaccounts.co.uk>
2018-07-21 15:24:02 +0100

    added index.js for node module entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 module.exports = {
     entry: "./src/index.js",
+    mode:"development",
     output: {
         path: __dirname + "/dist",
         filename: "bundle.js"
