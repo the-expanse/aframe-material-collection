6ef0a796872ba65f0c24e8ef744d559e22ba9c31
Shane Harris <shane@xactaccounts.co.uk>
2018-08-11 12:55:35 +0100

    dist files generated

--- aframe-material-collection.js
+++ aframe-material-collection.js
@@ -88,7 +88,7 @@
 /* 0 */
 /***/ (function(module) {
 
-module.exports = {"name":"aframe-material-collection","version":"0.2.26","description":"Material UI based primitives and components for use in your aframe projects.","homepage":"https://github.com/shaneharris/aframe-material-collection","keywords":["AFRAME","UI","Material"],"scripts":{"start":"webpack-dev-server --mode development","build":"webpack --mode production"},"repository":{"type":"git","url":"git@github.com:shaneharris/aframe-material-collection.git"},"bugs":{"url":"https://github.com/shaneharris/aframe-material-collection/issues"},"devDependencies":{"uglifyjs-webpack-plugin":"^1.2.7","webpack":"^4.16.1","webpack-cli":"^3.1.0","webpack-dev-server":"^3.1.4"},"author":"Shane Harris","license":"MIT","dependencies":{}};
+module.exports = {"name":"aframe-material-collection","version":"0.2.27","description":"Material UI based primitives and components for use in your aframe projects.","homepage":"https://github.com/shaneharris/aframe-material-collection","keywords":["AFRAME","UI","Material"],"scripts":{"start":"webpack-dev-server --mode development","build":"webpack --mode production"},"repository":{"type":"git","url":"git@github.com:shaneharris/aframe-material-collection.git"},"bugs":{"url":"https://github.com/shaneharris/aframe-material-collection/issues"},"devDependencies":{"uglifyjs-webpack-plugin":"^1.2.7","webpack":"^4.16.1","webpack-cli":"^3.1.0","webpack-dev-server":"^3.1.4"},"author":"Shane Harris","license":"MIT","dependencies":{}};
 
 /***/ }),
 /* 1 */
@@ -1437,8 +1437,9 @@ module.exports = AFRAME.registerComponent('ui-scroll-pane', {
                 child.addEventListener('textfontset',()=>{
 
                     UI.utils.isChanging(this.el.sceneEl,this.el.object3D.uuid);
-                    setTimeout(()=>UI.utils.stoppedChanging(this.el.object3D.uuid),100);
                     traverse();
+
+                    setTimeout(()=>UI.utils.stoppedChanging(this.el.object3D.uuid),200);
                 })
             }else{
                 traverse();
