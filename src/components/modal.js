cc923292d1601a95a7f0484b571000cce9a15516
Shane Harris <shane@xactaccounts.co.uk>
2018-08-14 15:12:31 +0100

    added open/close methods to modal component

--- src/components/modal.js
+++ src/components/modal.js
@@ -15,32 +15,39 @@ module.exports = AFRAME.registerComponent('ui-modal', {
             // Get the modal panel to be able to animate its scale on open/close.
             this.modalPanel = document.querySelector(this.data.modal.getAttribute('ui-panel'));
 
-            let mainComponents = this.data.main.components;
-            let modalComponents = this.data.modal.components;
+            this.mainComponents = this.data.main.components;
+            this.modalComponents = this.data.modal.components;
             // Pause rendering of modal until opened.
-            if(modalComponents&&modalComponents['ui-renderer']){
-                modalComponents['ui-renderer'].pause();
+            if(this.modalComponents&&this.modalComponents['ui-renderer']){
+                this.modalComponents['ui-renderer'].pause();
             }
             // Setup close listeners for anything with the class close-modal.
             let close_buttons = this.data.modal.querySelectorAll('.close-modal');
             for(let i = 0; i < close_buttons.length; i++ ){
                 let close_button = close_buttons[i];
                 close_button.addEventListener('mousedown',()=>{
-                    // Pause the modal rendering and play the main rendering again.
-                    modalComponents['ui-renderer'].pause();
-                    mainComponents['ui-renderer'].playRender();
-                    this.tweenModalScale(1,0.0000001);
+                    this.close();
                 });
             }
             // Add click handler for opening the modal, pause the main render screen and play the modal renderer
             this.el.addEventListener('mousedown',()=>{
-                if(mainComponents&&mainComponents['ui-renderer']){
-                    mainComponents['ui-renderer'].pauseRender();
-                    this.tweenModalScale(0.0000001,1);
-                    modalComponents['ui-renderer'].play();
-                }
+                this.open();
             });
+            this.data.main.modal = this;
         }
+    },
+    open(){
+        if(this.mainComponents&&this.mainComponents['ui-renderer']){
+            this.mainComponents['ui-renderer'].pauseRender();
+            this.tweenModalScale(0.0000001,1);
+            this.modalComponents['ui-renderer'].play();
+        }
+    },
+    close(){
+        // Pause the modal rendering and play the main rendering again.
+        this.modalComponents['ui-renderer'].pause();
+        this.mainComponents['ui-renderer'].playRender();
+        this.tweenModalScale(1,0.0000001);
     },
     tweenModalScale(from,to){
         return new Promise(r=>{
